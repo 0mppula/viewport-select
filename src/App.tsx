5e2676f7fc53d83afa86f5430a196bffad4f0c73
Omar Kraidié <omppula_1@hotmail.com>
2023-12-24 15:43:32 +0200

    UI fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import SelectArea from './components/SelectArea';
 
 function App() {
 	return (
-		<div className="wrapper">
-			<h1>Viewport Select</h1>
+		<>
 			<SelectArea />
-		</div>
+		</>
 	);
 }
 
